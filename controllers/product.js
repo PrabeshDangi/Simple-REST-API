bdc9f193024789e854670ad6be2fb2840a93b71c
Prabesh Dangi <dangiprabesh58@gmail.com>
2023-10-21 19:26:50 +0545

    Single query filter handled

--- controllers/product.js
+++ controllers/product.js
@@ -2,12 +2,18 @@ const Product=require('../models/productModel');
 
 exports.getAllProducts=async(req,res)=>{
     try{
-    const allProducts=await Product.find(req.query);
+        const {company}=req.query;
+        const queryObject={}
+        if(company){
+            queryObject.company=company;
+        }
+    const allProducts=await Product.find(queryObject);
     res.status(200).json({
         Number:allProducts.length,
         status:"Success",
         data:allProducts
     })
+    console.log(queryObject)
     }
     catch(err){
         res.status(400).json({
